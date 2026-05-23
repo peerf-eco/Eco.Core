f427b0f57073de26f16c12e4d1579a3cca00fb31
LeTim42 <132195594+LeTim42@users.noreply.github.com>
2026-05-23 23:59:00 +0300

    Fix test name

--- Eco.Java2ACOM/UnitTestFiles/SourceFiles/EcoTest.java
+++ Eco.Java2ACOM/UnitTestFiles/SourceFiles/EcoTest.java
@@ -210,12 +210,12 @@ public class EcoTest implements EcoApp {
     }
 
     private short TestComponent() {
-        System.out.println("\n==== Test C ====");
+        System.out.println("\n==== Test Component ====");
 
         iTestC = new IEcoTest1Native();
         short result = iBus.QueryComponent(IdEcoTestC1.CID, new IEcoUnknownNative(), IEcoTest1.IID, new Pointer<>((iTestC)));
         if (result != 0) return result;
-        System.out.println("IEcoTestC1 QueryComponent: OK\n");
+        System.out.println("IEcoTest1 QueryComponent: OK\n");
 
         /* ==== TestInt8 ==== */
         byte int8_in = 12;
