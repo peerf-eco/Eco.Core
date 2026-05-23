db94d50e56f6b5501aa6e9a79e50e1ca39d4c0f4
LeTim42 <132195594+LeTim42@users.noreply.github.com>
2026-05-23 23:23:54 +0300

    Update UGUID class

--- Eco.Java2ACOM/SharedFiles/Eco/Core/UGUID.java
+++ Eco.Java2ACOM/SharedFiles/Eco/Core/UGUID.java
@@ -3,11 +3,15 @@ package Eco.Core;
 import java.util.Arrays;
 
 public class UGUID {
-    byte Preamble;
-    byte Length;
-    byte[] Data;
+    public byte Preamble;
+    public byte Length;
+    public byte[] Data;
 
-    public UGUID() {}
+    public UGUID() {
+        Preamble = 0;
+        Length = 0;
+        Data = new byte[]{};
+    }
 
     public UGUID(byte Preamble, byte Length, byte... Data) {
         this.Preamble = Preamble;
@@ -17,5 +21,25 @@ public class UGUID {
 
     public boolean equals(UGUID other) {
         return Preamble == other.Preamble && Length == other.Length && Arrays.equals(Data, other.Data);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        for (byte i : Data) {
+            byte b = (byte) ((i & 0xFF) >> 4);
+            if (b < 10) {
+                result.append((char) (b + '0'));
+            } else {
+                result.append((char) ((b - 10) + 'A'));
+            }
+            b = (byte) (i & 0xF);
+            if (b < 10) {
+                result.append((char) (b + '0'));
+            } else {
+                result.append((char) ((b - 10) + 'A'));
+            }
+        }
+        return result.toString();
     }
 }
